1c64ae87840ce5de32e71849c1c5712ce26fb487
Naoki Ito <naoki.itoh.7kusa@gmail.com>
2015-04-10 00:56:06 +0900

    updaet

--- node_sample/src/express_server.js
+++ node_sample/src/express_server.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+
 var app = express();
 
-// Routing
+//app.use(express.bodyDecoder());
+
+app.register('.ejs', ejs);
+
 app.get('/', function(req, res) {
-  res.send('Hello world\n');
+  res.render('index.ejs');
+});
+
+app.post('/', function(req, res) {
+  res.render('result.ejs', {
+    locals: { message: req.body.url }
+  });
 });
 
 app.get(/^\/[0-9]+$/, function(req, res) {
--- node_sample/src/simple_server.js
+++ node_sample/src/simple_server.js
@@ -1,8 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world\n');
+  fs.readFile('index.html', function(err, content) {
+    if (err) {
+      throw err;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(content);
+  });
 }).listen(3000);
 
 console.log('Server running at http://localhost:3000/');
